90fab64e9ee052fedc5ba3f96129162027777029
Angela Choi <angelamchoi@gmail.com>
2021-06-15 16:08:14 -0400

    added functions

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
+print("----------------------")
+print("Let's play Py-Pac-Poe!")
+
+ num_wins = int(input("How many wins to play to?"))
+ 
  # Global variables
+ score = {'X': '0', 'O': '0', 'T':0}
  board = {}
  turn = 'X'
  winner = None
  
+ # Functions
  def init_game():
    # Use the global keyword to update global variables
    global board, turn
    board = {
    	'a1': None, `b1`: None, 'c1' None,
-   	# etc
+    'a2': None, `b2`: None, 'c3' None,
+    'a3': None, `b3`: None, 'c3' None,
    }
    turn = 'X'
+   winner = None
+
+def play_game():
+
+def take_turn():
+
+def print_board():
+
+def get_winner():
+
+
+def handle_winner():
 
